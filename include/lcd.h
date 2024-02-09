364a29b70267e5d18d0299ae9a1623e5e31b1309
Quwy <38657498+Quwy@users.noreply.github.com>
2024-02-09 02:16:12 +0200

    Add files via upload

---
+++ include/lcd.h
@@ -0,0 +1,28 @@
+/*********************************************
+Project : I2C to LCD Interface-Routine
+Port PCF8574 :  7  6  5  4  3  2  1  0
+               D7 D6 D5 D4 BL EN RW RS
+**********************************************/
+
+#ifndef __LCD_H__
+#define __LCD_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "defines.h"
+
+void lcd_init(const uint8_t address, const uint8_t columns, const bool backlight);
+void lcd_goto_xy(const uint8_t x, const uint8_t y);
+void lcd_clear(void);
+void lcd_putchar(const char c);
+void lcd_safe_puts(const char *str);
+void lcd_fast_puts(const char *const str);
+bool lcd_fast_puts_xy(const uint8_t x, const uint8_t y, const char *str);
+void lcd_backlight(const bool state, const bool instant);
+bool lcd_get_backlight(void);
+uint8_t lcd_get_width(void);
+uint8_t lcd_get_x(void);
+uint8_t lcd_get_y(void);
+
+#endif /* __LCD_H__ */
---
+++ include/twi.h
@@ -0,0 +1,25 @@
+#ifndef __TWI_H__
+#define __TWI_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "defines.h"
+
+#define PRT PORTB
+#define DDR DDRB
+#define PIN PINB
+#define SDA PINB0
+#define SCL PINB2
+
+typedef void (*twi_write_delay_callback_t)(const uint8_t, const uint8_t);
+
+void twi_init_master(void);
+void twi_start(void);
+void twi_stop(void);
+bool twi_write_byte(const uint8_t data);
+bool twi_write_bytes(const uint8_t *const data, const uint8_t size, const twi_write_delay_callback_t delay_callback);
+bool twi_write_begin(const uint8_t address);
+void twi_write_end(void);
+
+#endif /* __TWI_H__ */
